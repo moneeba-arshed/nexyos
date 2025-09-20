3e73c609e30fae6b1356fa529ca73c121e0f711c
YumnaHammad <yumnahammad@example.com>
2025-09-20 22:46:49 +0500

    Error Fix Iot Page

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ import Header from "./components/Header";
 import ChannelPage from "./pages/Partner/ChannelPage";
 import PartnerProgram from "./pages/Partner/ChannelPartner";
 import ProjectRegistration from "./pages/Partner/ProjectRegistration";
-import Iot from "./components/Channel/FindChannel/Iot";
+import Iot from "./pages/Partner/Iot";
 import About from "./components/Company/About";
 import Brand from "./components/Company/Brand";
 import Events from "./components/Company/Events";
