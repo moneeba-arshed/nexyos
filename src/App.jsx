de228b49684ba5b24fc994097632c9cd8f51afdc
YumnaHammad <yumnahammad@example.com>
2025-09-20 22:51:45 +0500

    Brand Error Fix

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ import PartnerProgram from "./pages/Partner/ChannelPartner";
 import ProjectRegistration from "./pages/Partner/ProjectRegistration";
 import Iot from "./pages/Partner/Iot";
 import About from "./pages/Company/About";
-import Brand from "./components/Company/Brand";
+import Brand from "./pages/Company/Brand";
 import Events from "./components/Company/Events";
 import BlogPage from "./components/Blog/BlogPage";
 import BlogDetailPage from "./components/Blog/BlogDetailPage";
